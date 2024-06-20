26d5ff061e2c4f79d070453af5b802611c8fba1a
kate <46572320+katelyynn@users.noreply.github.com>
2024-06-20 21:35:08 +0100

    feat: progressss

--- fm/bwaa.user.js
+++ fm/bwaa.user.js
@@ -6,13 +6,15 @@
 // @author       kate
 // @match        https://www.last.fm/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=last.fm
-// @updateURL    https://github.com/katelyynn/bleh/raw/uwu/fm/bleh.user.js
-// @downloadURL  https://github.com/katelyynn/bleh/raw/uwu/fm/bleh.user.js
+// @updateURL    https://github.com/katelyynn/bwaa/raw/uwu/fm/bwaa.user.js
+// @downloadURL  https://github.com/katelyynn/bwaa/raw/uwu/fm/bwaa.user.js
 // @run-at       document-body
 // @require      https://cdnjs.cloudflare.com/ajax/libs/showdown/2.1.0/showdown.min.js
 // @require      https://unpkg.com/@popperjs/core@2
 // @require      https://unpkg.com/tippy.js@6
 // ==/UserScript==
+
+console.info('bwaa - beginning to load');
 
 let version = '2024.0620';
 let lang = document.documentElement.getAttribute('lang');
@@ -29,11 +31,11 @@ const trans = {
     }
 }
 
-tippy.setDefaultProps({
+/*tippy.setDefaultProps({
     arrow: false,
     duration: [100, 300],
     delay: [null, 50]
-});
+});*/
 
 let settings_defaults = {
 
@@ -49,9 +51,10 @@ let bwaa_regex = new RegExp('^https://www\.last\.fm/[a-z]+/bwaa$');
     'use strict';
 
     auth = document.querySelector('a.auth-link img').getAttribute('alt');
-    initia();
+    console.info('bwaa - auth', auth);
+    bwaa();
 
-    function initia() {
+    function bwaa() {
         console.info('bwaa - starting up');
 
         // essentials
@@ -61,12 +64,130 @@ let bwaa_regex = new RegExp('^https://www\.last\.fm/[a-z]+/bwaa$');
             // start bwaa settings
         } else {
             // things that load when not in bwaa settings
+            bwaa_profiles();
         }
+
+        // last.fm is a single page application
+        const observer = new MutationObserver((mutations) => {
+            for (const mutation of mutations) {
+                for (const node of mutation.addedNodes) {
+                    if (node instanceof Element) {
+                        if (!node.hasAttribute('data-bwaa')) {
+                            node.setAttribute('data-bwaa', 'true');
+
+                            console.info('bwaa - bwaa\'ing');
+
+                            // essentials
+                            bwaa_load_header();
+
+                            if (window.location.href == bwaa_url || bwaa_regex.test(window.location.href)) {
+                                // start bwaa settings
+                            } else {
+                                // things that load when not in bwaa settings
+                                bwaa_profiles();
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
     }
 
 
     // header
     function bwaa_load_header() {
-
+        return 200;
     }
-});
+
+
+    // run on profile
+    function bwaa_profiles() {
+        console.info('bwaa - profiles');
+
+        // are we on a profile?
+        let profile_header = document.body.querySelector('.header--user');
+
+        if (profile_header == undefined)
+            return;
+
+        console.info('bwaa - user is on a profile');
+
+        // are we on the overview page?
+        let profile_header_overview = profile_header.classList.contains('header--overview');
+        console.info('bwaa - profile overview?', profile_header_overview);
+
+
+        if (profile_header_overview) {
+            // profile overview stuff
+
+            // remove the profile card-related stuff
+            let content_top = document.body.querySelector('.content-top');
+            if (!content_top.hasAttribute('data-bwaa')) {
+                content_top.setAttribute('data-bwaa', 'true');
+                content_top.style.setProperty('display', 'none');
+            }
+
+
+            // re-implement header
+            let col_main = document.querySelector('.col-main');
+            let recent_tracks = document.getElementById('recent-tracks-section'); // we will use this to append before it
+
+            // fetch some data from the header
+            let header_metadata = profile_header.querySelectorAll('.header-metadata-display p');
+            let header_user_data = {
+                avatar: profile_header.querySelector('.avatar img'),
+                name: profile_header.querySelector('.header-title a').textContent,
+                display_name: profile_header.querySelector('.header-title-display-name').textContent,
+                since: profile_header.querySelector('.header-scrobble-since').textContent,
+                scrobbles: header_metadata[0],
+                artists: header_metadata[1].querySelector('a'),
+                loved_tracks: header_metadata[2].querySelector('a')
+            }
+
+            if (recent_tracks.hasAttribute('data-bwaa'))
+                return;
+            recent_tracks.setAttribute('data-bwaa', 'true');
+
+            let new_header = document.createElement('section');
+            new_header.classList.add('profile-header-section');
+            new_header.innerHTML = (`
+                <div class="badge-avatar">
+                    <img src="${header_user_data.avatar.getAttribute('src')}" alt="${header_user_data.avatar.getAttribute('alt')}">
+                </div>
+                <div class="badge-info">
+                    <h1>${header_user_data.name}</h1>
+                    <div class="user-info">
+                        <div class="top">
+                            <strong>${header_user_data.display_name}</strong>, cutensilly.org
+                        </div>
+                        <div class="bottom user-last-seen">
+                        Last seen: {}
+                        </div>
+                    </div>
+                    <div class="user-data">
+                        <div class="user-plays">
+                            <div class="count">
+                                {} plays
+                            </div>
+                            <div class="since">
+                                since {}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="user-activity">
+                        <a>{} Loved Tracks</a> | <a href="${header_user_data.artists.getAttribute('href')}">${header_user_data.artists.textContent} Artists</a> | <a>Shoutbox</a>
+                    </div>
+                </div>
+            `);
+
+            col_main.insertBefore(new_header, recent_tracks);
+        } else {
+            // profile non-overview stuff
+        }
+    }
+})();
