c5772a93a20904d2510e8cdecc639983c8a2ee49
kate <46572320+katelyynn@users.noreply.github.com>
2024-09-08 16:11:39 +0100

    chore: bump

--- fm/bwaa.user.js
+++ fm/bwaa.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bwaa
 // @namespace    http://last.fm/
-// @version      2024.0907
+// @version      2024.0908
 // @description  bwaaaaaaa
 // @author       kate
 // @match        https://www.last.fm/*
@@ -17,7 +17,7 @@
 console.info('bwaa - beginning to load');
 
 let version = {
-    build: '2024.0907',
+    build: '2024.0908',
     sku: 'sweet'
 }
 
